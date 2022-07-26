83fd0b05521b74ab9b96ac28663bfceecbee518b
zmd <maratz375@gmail.com>
2022-07-26 10:01:30 +0500

    scrapy_products
    отредактировал productsparser/items.py
    в класс ProductsparserItem
    добавил:
    _id = scrapy.Field()
    product_url = scrapy.Field()
    
    title_prod = scrapy.Field()
    
    old_price = scrapy.Field()
    old_price_currency = scrapy.Field()
    
    price = scrapy.Field()
    price_currency = scrapy.Field()
    
    measure = scrapy.Field()
    
    spec_dict = scrapy.Field()
    
    list_images = scrapy.Field()
    list_big_images = scrapy.Field()

--- productsparser/items.py
+++ productsparser/items.py
@@ -9,4 +9,20 @@ import scrapy
 class ProductsparserItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    _id = scrapy.Field()
+    product_url = scrapy.Field()
+
+    title_prod = scrapy.Field()
+
+    old_price = scrapy.Field()
+    old_price_currency = scrapy.Field()
+
+    price = scrapy.Field()
+    price_currency = scrapy.Field()
+
+    measure = scrapy.Field()
+
+    spec_dict = scrapy.Field()
+
+    list_images = scrapy.Field()
+    list_big_images = scrapy.Field()
